94b607705cde5fbac8d4d56256cb5db8fd5d4f57
jesucuad-xyn <jcuadrado76@gmail.com>
2025-08-03 13:24:05 +0200

    Java errors

--- user-profile-app/src/main/java/com/example/model/User.java
+++ user-profile-app/src/main/java/com/example/model/User.java
@@ -1,9 +1,10 @@
 package com.example.model;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import jakarta.persistence.*;
+//import javax.persistence.Entity;
+//import javax.persistence.GeneratedValue;
+//import javax.persistence.GenerationType;
+//import javax.persistence.Id;
 
 @Entity
 public class User {
